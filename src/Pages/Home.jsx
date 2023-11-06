076be97d13bd09bc5ee34ef374c2fa02d181542c
Ador Shahriar <ador.shahriar@gmail.com>
2023-11-07 01:04:51 +0600

    recent blog section in home page added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Components/Banner';
 import NewsLetter from '../Components/NewsLetter';
+import RecentBlogs from '../Components/RecentBlogs';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <RecentBlogs></RecentBlogs>
             <NewsLetter></NewsLetter>
         </div>
     );
