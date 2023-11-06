5c2e4823e654a8092c77b0c0806a5f0a6aa771ba
Ador Shahriar <ador.shahriar@gmail.com>
2023-11-07 03:20:04 +0600

    wishlist page added, delete button functionality added

--- src/Components/BlogCard.jsx
+++ src/Components/BlogCard.jsx
@@ -1,26 +1,58 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import useAuth from "../Hooks/useAuth";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const BlogCard = ({ blog }) => {
+  const { user } = useAuth();
   const { _id, title, image, category, short, long } = blog;
-  const navigate = useNavigate()
-  const handleClickDetails = id => {
-    navigate(`/blogs/${id}`)
-  }
+  const navigate = useNavigate();
+  const handleClickDetails = (id) => {
+    navigate(`/blogs/${id}`);
+  };
+  const addToWishlist = () => {
+    const blogId = _id;
+    const userEmail = user.email;
+    const wishList = { blogId, userEmail };
+    axios.post("http://localhost:5000/wishlist", wishList).then((res) => {
+      if (res.data.insertedId) {
+        toast("Blog added to wishlist successfully");
+      }
+    });
+  };
   return (
     <div>
       <div className="card card-compact  h-[500px] bg-base-100 shadow-xl">
-  <figure><img className="w-full h-[250px] object-cover" src={image} /></figure>
-  <div className="card-body">
-    <h2 className="card-title">{title}</h2>
-    <p>{short}</p>
-    <p>Category:<span className="bg-orange-600 py-1 px-2 text-white rounded-lg ml-3"> {category}</span></p>
-    <div className="card-actions justify-end">
-        <button className="btn normal-case font-medium">Add to Wishlist</button>
-      <button onClick={() => handleClickDetails(_id)} className="btn btn-primary normal-case font-medium">Details</button>
-    </div>
-  </div>
-</div>
+        <figure>
+          <img className="w-full h-[250px] object-cover" src={image} />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{title}</h2>
+          <p>{short}</p>
+          <p>
+            Category:
+            <span className="bg-orange-600 py-1 px-2 text-white rounded-lg ml-3">
+              {" "}
+              {category}
+            </span>
+          </p>
+          <div className="card-actions justify-end">
+            <button
+              onClick={addToWishlist}
+              className="btn normal-case font-medium"
+            >
+              Add to Wishlist
+            </button>
+            <button
+              onClick={() => handleClickDetails(_id)}
+              className="btn btn-primary normal-case font-medium"
+            >
+              Details
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Wishlist.jsx
+++ src/Pages/Wishlist.jsx
@@ -1,11 +1,84 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import React, { useEffect } from "react";
+import useAuth from "../Hooks/useAuth";
+import WishListCard from "../Components/WishListCard";
+import { toast } from "react-toastify";
 
 const Wishlist = () => {
+  const { user } = useAuth();
+  const allBlogs = useQuery({
+    queryKey: ["blogsData"],
+    queryFn: () =>
+      axios.get("http://localhost:5000/allblogs").then((res) => res.data),
+  });
+
+  const emailQuery = useQuery({
+    queryKey: ["wishlistData"],
+    queryFn: () =>
+      axios.get(`http://localhost:5000/wishlist`).then((res) => res.data),
+  });
+
+  if (allBlogs.isLoading) {
     return (
-        <div>
-            wishlist
-        </div>
+      <div>
+        <progress className="progress w-56" value="100" max="100"></progress>
+      </div>
     );
+  }
+
+  if (emailQuery.isLoading) {
+    return (
+      <div>
+        <progress className="progress w-56" value="100" max="100"></progress>
+      </div>
+    );
+  }
+  const allBlogsData = allBlogs?.data;
+  const wishListData = emailQuery?.data;
+  const userWishList = wishListData?.filter(
+    (item) => item.userEmail === user?.email
+  );
+  const wishListIds = userWishList?.map((item) => item.blogId);
+  const wishListBlogs = []
+  for (let item of wishListIds) {
+    for (let blog of allBlogsData) {
+      if (blog._id == item) {
+        wishListBlogs.push(blog);
+      }
+    }
+  }
+
+  const handleDelete = (id) => {
+    const email = user.email;
+    const myRef = { email };
+    console.log(email)
+    fetch(`http://localhost:5000/wishlist/${id}`, {
+        method: "DELETE",
+        headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(myRef),
+    })
+    .then(res => res.json())
+    .then(data=> {
+        if(data.deletedCount){
+            toast('deleted successfully');
+            emailQuery.refetch()
+        }
+    })
+}
+
+  return (
+    <div className="max-w-[1260px] mx-auto my-20">
+      <h2 className="text-center font-bold text-3xl">Wishlist</h2>
+      <div className="grid grid-cols-1 lg:grid-cols-3 justify-items-center gap-5 my-10">
+        {wishListBlogs.map((blog, index) => (
+          <WishListCard key={index} blog={blog} handleDelete={handleDelete}></WishListCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Wishlist;
