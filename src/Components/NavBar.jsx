1b80eb2fab87013136883d9eb2586bd4a0787dfe
Ador Shahriar <ador.shahriar@gmail.com>
2023-11-08 22:14:43 +0600

    some style updated

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,52 +3,46 @@ import { Link, NavLink } from "react-router-dom";
 import useAuth from "../Hooks/useAuth";
 import { toast } from "react-toastify";
 
-
-
 const NavBar = () => {
-  
-  const { user, logOut } = useAuth()
+  const { user, logOut } = useAuth();
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const handleLogOut = () => {
     logOut()
       .then(() => toast("logged out"))
       .catch((error) => console.error(error.message));
-
-
-
-   
-   
   };
   const navLinks = (
     <>
       <li>
         <NavLink to="/">Home</NavLink>
       </li>
-      {
-        user &&  <li>
-        <NavLink to="/addblog">Add Blog</NavLink>
-      </li>
-      }
-     
+      {user && (
+        <li>
+          <NavLink to="/addblog">Add Blog</NavLink>
+        </li>
+      )}
+
       <li>
         <NavLink to="/allblogs">All Blogs</NavLink>
       </li>
       <li>
         <NavLink to="/featuredblogs">Featured Blogs</NavLink>
       </li>
-      {
-        user && <li>
-        <NavLink to="/wishlist">Wishlist</NavLink>
-      </li>
-      }
-      
+      {user && (
+        <li>
+          <NavLink to="/wishlist">Wishlist</NavLink>
+        </li>
+      )}
     </>
   );
   return (
     <div>
       <div className="navbar bg-base-100 max-w-[1260px] mx-auto">
         <div className="navbar-start">
-          <div className="dropdown" onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+          <div
+            className="dropdown"
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+          >
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -65,19 +59,20 @@ const NavBar = () => {
                 />
               </svg>
             </label>
-            {
-              isDropdownOpen && (
-                <ul
+            {isDropdownOpen && (
+              <ul
                 tabIndex={0}
                 className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
               >
                 {navLinks}
               </ul>
-              )
-            }
-          
+            )}
           </div>
-          <img className="md:w-12 w-8" src="https://i.ibb.co/VT2mhg5/icons8-blog-96.png" alt="" />
+          <img
+            className="md:w-12 w-8"
+            src="https://i.ibb.co/VT2mhg5/icons8-blog-96.png"
+            alt=""
+          />
           <a className="normal-case font-bold lg:text-lg">The Blog Hub</a>
         </div>
         <div className="navbar-center hidden lg:flex">
@@ -91,17 +86,29 @@ const NavBar = () => {
               src={user.photoURL}
               alt=""
             />
-            <Link onClick={handleLogOut} className="btn font-bold">
+            <Link
+              onClick={handleLogOut}
+              className="btn font-bold normal-case md:text-lg"
+            >
               Log Out
             </Link>
           </div>
         ) : (
           <div className="navbar-end">
-            <Link className="btn mr-4" to="/login">Log In</Link>
-          <Link className="btn" to="/register">Register</Link>
+            <Link
+              className="btn mr-4 normal-case font-medium md:text-lg"
+              to="/login"
+            >
+              Log In
+            </Link>
+            <Link
+              className="btn normal-case font-medium md:text-lg"
+              to="/register"
+            >
+              Register
+            </Link>
           </div>
         )}
-
       </div>
     </div>
   );
